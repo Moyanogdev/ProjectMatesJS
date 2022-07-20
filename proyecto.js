4d6290a22e446286e6b9fddba90ed530040d3bec
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-07-20 14:54:29 -0300

    Avances Proyecto JS

--- proyecto.js
+++ proyecto.js
@@ -1,5 +1,5 @@
 
-
+// Función de Bienvenida al usuario.
 function bienvenida(){
 
     let usuario = document.getElementById("nombre_usuario");
@@ -10,12 +10,17 @@ function bienvenida(){
     console.log("Apellido: " , apellido.value);
     console.log("Email registrado: " , email.value);
 
-    let msj = document.createElement("h1");
-    msj.innerText = "BIENVENIDO/A";
-
-    mensaje.append(msj);
+    Swal.fire({
+        icon: "success",
+        text: "BIENVENIDO A MOYA MATES",
+        imageUrl: './img/logo.png',
+        imageWidth: 250,
+        imageHeight: 150,
+        imageAlt: 'Logo de la marca',
+    });
 }
 
+// Función Remover datos cargados del usuario. Nota: No funciona correctamente.
 function remover(){
     let usuario = document.getElementById("nombre_usuario");
     let apellido = document.getElementById("apellido_usuario");
@@ -25,14 +30,8 @@ function remover(){
     console.log("Se ha eliminado el apellido: " , apellido.value);
     console.log("Email eliminado: " , email.value);
 
-    usuario.remove();
-    apellido.remove();
-    email.remove();
 
 }
-
-/* La función remover, elimina los nodos existentes. es solo demostración
-ya que no registra funcionalidad para el simulador.*/
 
 let boton_uno = document.getElementById("boton_ingresar");
 let boton_dos = document.getElementById("boton_borrar");
@@ -44,18 +43,22 @@ boton_uno.addEventListener("click" , bienvenida);
 
 
 
-// CARRITO
-
+// Carrito de compras
 
 let botones_compra = document.querySelectorAll(".botonAgregar");
+let boton_comprar = document.getElementById("botonComprar");
+let boton_vaciar = document.getElementById("botonVaciar");
 let carrito = [];
 let carrito_storage = [];
+
+boton_vaciar.addEventListener("click" , vaciar_carrito);
 
 for( let boton of botones_compra){
 
     boton.addEventListener("click", agregar_producto);
 }
 
+// Función para agregar los productos.
 function agregar_producto(e){
 
     let hijo = e.target;
@@ -73,18 +76,29 @@ function agregar_producto(e){
         cantidad:1
     };
 
-    carrito.push(producto);
+    carrito.push();
 
     let producto_JSON = JSON.stringify(producto);
     carrito_storage.push(producto_JSON);
 
     localStorage.setItem("producto" , carrito_storage);
 
+    Toastify({
+        text: "Producto agregado al carrito",
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style:{
+            background:"green",
+            fontSize:"15px"
+        },
+    }).showToast();
+
     ver_carrito(producto);
 }
 
 
-/* Renderizar los productos en el carrito de compras. */
+// Renderizar los productos en el carrito de compras.
 function ver_carrito(producto){
 
     let fila = document.createElement("tr");
@@ -107,103 +121,40 @@ function ver_carrito(producto){
 
 }
 
-
+// Función para eliminar productos individualmente.
 function borrar_producto(e){
 
     let hijo = e.target;
     let abuelo = hijo.parentNode.parentNode;
     abuelo.remove();
+    Toastify({
+        text: "Producto eliminado del carrito",
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style:{
+            background:"red",
+            fontSize:"15px"
+        },
+    }).showToast();
 }
 
+// Función para vaciar el carrito de compras.
+function vaciar_carrito(){
+    
+    $("#carritoTbody tr").remove();
+    Swal.fire({
+        icon: "warning",
+        text: "LOS PRODUCTOS FUERON ELIMINADOS",
+        imageUrl: './img/logo.png',
+        imageWidth: 250,
+        imageHeight: 150,
+        imageAlt: 'Logo de la marca',
+    });
+    
 
-
-/* OBJETO PRODUCTO
-
-class Producto{
-    constructor(id, nombre , precio , stock){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    get_datos(){
-        console.log("Datos Producto");
-        console.log("Nombre: " , this.nombre);
-        console.log("Precio: " , this.precio);
-        console.log("Stock: " , this.stock);
-    }
-
-    get_stock(){
-        if(this.stock <= 5){
-            console.log("ALERTA POCO STOCK: ",  this.stock);
-        }
-        return this.stock;
-    }
-
-    set_stock_venta(cantidad){
-        if(this.stock > 0){
-            this.stock = this.stock - cantidad;
-        }
-        else if(this.stock <= 0){
-            alert("Producto fuera de stock");
-            console.log("Producto fuera de stock");
-        }
-    }
 }
 
-*/
-
-
-
-/* FUNCION DE CALCULO DE CUOTAS
-
-
-  function calculo_cuotas (suma_total , cuotas){
-
-    cuotas = parseInt( prompt("Ingrese cuotas a pagar: 1, 3, 6 o 12"));
-    cuotas = parseInt(cuotas);
-
-    if (cuotas == 1){
-        parseInt(suma_total);
-        alert("Total a pagar en una cuota: " + suma_total);
-        console.log("Total a pagar en una cuota: " , suma_total);
-        console.log("Gracias por su compra.");
-        return suma_total;
-    }
-
-    else if (cuotas == 3){
-        parseInt(suma_total);
-        alert("Total a pagar en 3 cuotas: " + suma_total * 1.2);
-        console.log("Total a pagar en 3 cuotas: " , suma_total * 1.2);
-        console.log("Gracias por su compra.");
-        return suma_total * 1.2;
-    }
-
-    else if (cuotas == 6){
-        parseInt(suma_total);
-        alert("Total a pagar en 6 cuotas: " + suma_total * 1.5);
-        console.log("Total a pagar en 6 cuotas: " , suma_total * 1.5);
-        console.log("Gracias por su compra.");
-        return suma_total * 1.5;
-    }
-
-    else if (cuotas == 12){
-        parseInt(suma_total);
-        alert("Total a pagar en 12 cuotas:  " + suma_total * 1.8);
-        console.log("Total a pagar en 12 cuotas: " , suma_total * 1.8);
-        console.log("Gracias por su compra.");
-        return suma_total * 1.8;
-    }
-
-    else{
-        alert("INGRESE UN DATO CORRECTO");
-        console.log("No ingresó un dato correcto");
-        calculo_cuotas(suma_total , cuotas);
-    }
-}
-
-*/
 
 
 
